60448ccff18b0173de9216de94fcaaafdaf1a504
Nidhi <129270499+Chaudhary-Nidhi@users.noreply.github.com>
2023-11-29 10:38:43 +0000

    type conversion done

---
+++ basics/typeConversion.js
@@ -0,0 +1,39 @@
+let value=3
+let negativeValue=-3
+//console.log(negativeValue)
+
+//console.log(1+2+"3");
+//console.log("1"+2+3);
+
+//console.log("2">1);
+//console.log("2">4);
+
+//console.log(null>0);
+//console.log(null==0);
+//console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//let a="One";
+//let b=a;
+//b="Two";
+//console.log(a);
+//console.log(b);
+
+//const name="Nidhi";
+//const age=21;
+//console.log("My name is "+name+" and my age is "+age);
+//console.log(`My name is ${name} and my age is ${age}`);
+
+//const accountId = 144553
+//let accountEmail = "nidhi@google.com"
+//var accountPassword = "12345"
+//accountCity = "Jaipur"
+//let accountState;
+//console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//console.log(typeof accountPassword);
+//console.log(typeof undefined); 
+//console.log(typeof null);
